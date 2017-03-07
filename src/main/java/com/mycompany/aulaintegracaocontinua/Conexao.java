6e8a5a554c6a37cedab2e7d6dfe88367bdb07044
washington-pc <washington-pc@DESKTOP-LGMHQ19.localdomain>
2017-03-07 09:42:41 -0300

    aula integracao

--- src/main/java/com/mycompany/aulaintegracaocontinua/Conexao.java
+++ src/main/java/com/mycompany/aulaintegracaocontinua/Conexao.java
@@ -5,13 +5,9 @@
  */
 package com.mycompany.aulaintegracaocontinua;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
